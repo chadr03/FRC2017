2c9681d43a36ea53b5fb6e3f5a25479c5d6d3d86
Chad Robertson <chadr03@gmail.com>
2019-02-12 17:13:39 -0600

    MergeTest

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -11,7 +11,7 @@ package frc.robot;
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
  * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
+ * floating around. CR
  */
 public class RobotMap {
   // For example to map the left and right motors, you could define the
